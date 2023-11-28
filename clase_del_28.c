c4ae345b000b7e11186bb5531ddb1ff2b74f0c95
javier <javierantonordonez@gmail.com>
2023-11-28 13:23:09 +0100

    apuntes de clase

--- clase_del_28.c
+++ clase_del_28.c
@@ -3,6 +3,15 @@
 
 #include <stdio.h>
 
+void ejemplo(void){
+    
+    int var=7;
+    int *ptr;
+    ptr=&var;
+
+    printf("%d", *ptr);
+}
+
 int mi_array[]={1,23,17,4,-5,100};
 int *ptr;
 int ejemplo1(void){
@@ -16,20 +25,46 @@ int ejemplo1(void){
     }
 }
 
-void ejemplo2(void){
+/*void ejemplo2(void){
 
     void ptr
-// si creamos un puntero con void se adaptara a la variable que se apunte
-    return;
+    // si creamos un puntero con void se adaptara a la variable que se apunte
+    //return;
+}*/
+
+int arr[10]={3,6,1,2,3,8,4,1,7,2};
+void doubble(int a[], int n){
+    int i,j,t;
+    for(i=n-1;i>=0;i--){
+        for(j=1; j<=0; i--){
+            if (a[j-1]>a[j]){
+                t=a[j-1];
+                a[j-1]=a[j];
+                a[j]=t;
+            }
+        }
+    }    
+    //return;
+}
+
+int ejemplo3(){
+    int i;
+    printf("\n");
+    for(i=0;i<10;i++){
+        printf("%d",arr[i]);
+    }
+    doubble(arr, 10);
+    printf("\n");
+    for(i=0;i<10;i++){
+        printf("%d",arr[i]);
+    }
+    return 0;
 }
 
 int main(void){
-
-    int var=7;
-    int *ptr;
-    ptr=&var;
-
-    printf("%d", *ptr);
-    ejemplo1();
+    //ejemplo();
+    //ejemplo1();
+    //ejemplo2();
+    ejemplo3();
     return 0;
 }
