a4a07578320db94a520488fdf8cae7f4108e9249
javier <javierantonordonez@gmail.com>
2023-11-29 22:39:50 +0100

    Ejercicio 1

---
+++ ejercios1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+void main(){
+
+    //Quiero que me hagas un codigo en donde incluyas los siguientes elementos
+    /*  Recibe un numero.
+        Quiero que en funcion del numero que se haya introducido pidas esa cantidas de letras del abecedario.
+            Ejemplo:
+                Numero --> 5
+                Letras --> 'a' 'b' 'c' 'd' 'e'
+
+        Introduciomos otro valor entero con un maximo de 100.
+        Luego quiero que hagas un bucle que se cumpla minimo una vez. 
+        Se ejecutara siempre que el elemento sea mayor que 50
+        En donde a dentro habra una seccion de codigo que se ejecutara solo cuando el valor del numero introducido sea mayor que 50
+        imprimiendo "El numero [valor] es mayor que 50"
+        Quiero que a ese bucle se le vaya restando un valor en cada vuelta.
+            Ejemplo:
+                Valor --> 55
+                Salida --> El numero 55 es mayor que 50
+                           El numero 54 es mayor que 50
+                           .
+                           .
+                           .
+                           El numero 50 es mayor que 50    
+    */
+
+}
