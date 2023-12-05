8060779648f473a4969264d3cdd0903bd19f8529
javier <javierantonordonez@gmail.com>
2023-12-05 12:27:19 +0100

    ejercicio 3

---
+++ ejercicio3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void main(void){
+    /*
+    Vamos a practicar las arrays.
+    Si no sabes lo que son preguntamelo y te lo explico.
+    Pero como explicacion rapido veamoslo como una calle con muchos parkings 
+    
+    |coche|coche|coche|coche|coche|coche|coche|coche|coche|coche|coche|coche|coche|coche|coche|
+
+    en cada lugar solo entra una variable o coche.
+
+    Quiero que me hagas una array que contenga cada letra del abecedario en orden alfavetico.
+
+    a,b,c,d,e,f,g,h,i,j,k,l,m,n,ñ,o,p,q,r,s,t,e,v,w,x,y,z
+
+    luego quiero que me imprimas el 5 elemento 
+    el 9 el 11 y si se puede el 35 elemento
+
+    Extra que me imprimas toda la array de comienzo a final y de final a comienzo    
+    
+    */
+}
